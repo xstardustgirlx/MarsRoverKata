989973c66cd5f5a848224b2ace666ff26b314dd9
Lenny3000 <91618917+Lenny3000@users.noreply.github.com>
2022-05-28 15:14:39 -0400

    added splitIndex and substring

--- textParser.js
+++ textParser.js
@@ -1,5 +1,11 @@
 function textParser(inputString) {
-    return 5;
+    if (inputString.indexOf(' ') != -1){
+        const splitIndex = inputString.indexOf(' ');
+        const firstPart = inputString.substring(0,splitIndex);
+        return [Number(firstPart)];
+    }
+        
+    return [Number(inputString)];
 }
 
 module.exports = textParser;
--- textParser.test.js
+++ textParser.test.js
@@ -1,5 +1,11 @@
 const parseText = require('./textParser');
 
 test('Should parse first number from text', () => {
-    expect(parseText('5')).toEqual(5);
+    expect(parseText('5')[0]).toEqual(5);
+});
+test('Should parse 3 from 3', () => {
+    expect(parseText('3')[0]).toEqual(3);
+});
+test('Should parse first number from text', () => {
+    expect(parseText('5 5')[0]).toEqual(5);
 });
